8e8e6fa46829948c57370b1150bb582e1b60261e
GuShA11 <bgoncalo11@gmail.com>
2023-09-17 21:13:02 +0200

    fin sección 16

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -12,5 +15,20 @@ export class LayoutPageComponent {
     { label: 'Listado', icon: 'label', url: './list' },
     { label: 'Añadir', icon: 'add', url: './new-hero' },
     { label: 'Buscar', icon: 'search', url: './search' },
-  ]
+  ];
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    ) { }
+
+  get user(): User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout(): void {
+    this.authService.logOut();
+    this.router.navigate(['/auth/login'])
+  }
+
 }
